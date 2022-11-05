5794dce2349bbaf965a0691af983f5523c562a0e
guille <guiacosta0000@gmail.com>
2022-11-05 17:49:02 -0300

    user details in header and dashboard

--- src/app/pages/banco/pages/dashboard/dashboard.component.ts
+++ src/app/pages/banco/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { User } from '@core/model/interfaces';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { ChartOptions } from 'chart.js';
 import {animate, state, style, transition, trigger} from '@angular/animations';
+import { BaseServicesService } from '@core/services/base-service';
 @Component({
   selector: 'ab-dashboard',
   templateUrl: './dashboard.component.html',
@@ -17,16 +18,15 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 })
 export class DashboardComponent implements OnInit {
   hideCurrency: boolean = false;
-  currentUser: User = {
-    first_name: 'Sebas',
-    last_name: 'Prueba',
-    email: 'test@test.com',
-    password: '',
-    roleId: 1,
-    points: 5000,
+  dataUsuario:User = {
+    id:0,
+    first_name:'',
+    last_name:'',
+    email:'',
+    password:'',
+    points:0,
+    roleId:0
   }
-  completeName: string = this.currentUser.first_name + ' ' + this.currentUser.last_name;
-
 
   dataSource = ELEMENT_DATA;
   columnsToDisplay = ['operacion', 'monto', 'usuarioId', 'concepto', 'fecha'];
@@ -34,7 +34,8 @@ export class DashboardComponent implements OnInit {
   expandedElement!: PeriodicElement | null;
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private base:BaseServicesService
   ) { }
 
   public pieChartOptions: ChartOptions<'doughnut'> = {
@@ -56,12 +57,20 @@ export class DashboardComponent implements OnInit {
   public pieChartLegend = true;
   public pieChartPlugins = [];
   ngOnInit(): void {
+    this.usuario()
   }
 
   showCurrencyFn() {
     this.hideCurrency = !this.hideCurrency;
   }
-}
+  usuario(){
+       this.base.getPerfil().subscribe(data => {
+        this.dataUsuario = data
+        console.log(this.dataUsuario)
+       })
+    }
+    }
+
 
 export interface PeriodicElement {
   name: string;
