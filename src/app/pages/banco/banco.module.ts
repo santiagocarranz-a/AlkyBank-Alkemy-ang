03dea0f9a6b1a6d2f1778a99801e8cf6bbc406c7
Z3belek <you@example.com>
2022-11-06 19:23:04 -0300

    Update and fixed dashboard, divide in components

--- src/app/pages/banco/banco.module.ts
+++ src/app/pages/banco/banco.module.ts
@@ -7,12 +7,22 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NgChartsModule } from 'ng2-charts';
 import { UsuarioPerfilComponent } from './pages/usuario-perfil/usuario-perfil.component';
 import { BalanceChartComponent } from './pages/balance/balance-chart/balance-chart.component';
+import { ProfileComponent } from './pages/dashboard/components/profile/profile.component';
+import { BankAccountComponent } from './pages/dashboard/components/bank-account/bank-account.component';
+import { PieChartComponent } from './pages/dashboard/components/pie-chart/pie-chart.component';
+import { LineChartComponent } from './pages/dashboard/components/line-chart/line-chart.component';
+import { AddBankAccountComponent } from './pages/dashboard/components/bank-account/components/add-bank-account/add-bank-account.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     UsuarioPerfilComponent,
-    BalanceChartComponent
+    BalanceChartComponent,
+ProfileComponent,
+    BankAccountComponent,
+    PieChartComponent,
+    LineChartComponent,
+    AddBankAccountComponent
   ],
   imports: [
     CommonModule,
