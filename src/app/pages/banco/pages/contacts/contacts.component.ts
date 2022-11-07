1556a595b9223221e0aafa6db06251eb01782daf
aaron2494 <aaron.e.francolino@gmail.com>
2022-11-06 21:19:34 -0300

    paginator incomplete

--- src/app/pages/banco/pages/contacts/contacts.component.ts
+++ src/app/pages/banco/pages/contacts/contacts.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Route, Router } from '@angular/router';
 import { User } from '@core/model/interfaces';
 import { BaseServicesService } from '@core/services/base-service';
-
+import { environment } from '@env/environment';
 @Component({
   selector: 'ab-contacts',
   templateUrl: './contacts.component.html',
@@ -9,16 +12,29 @@ import { BaseServicesService } from '@core/services/base-service';
 })
 export class ContactsComponent implements OnInit {
 user:User[]=[]
-  constructor(private base:BaseServicesService) { }
+apibase=environment.apiBase
+length = 100;
+pageSize = 10;
+pageSizeOptions: number[] = [5, 10, 25, 100];
+  pageEvent: PageEvent = new PageEvent;
+  constructor(private base:BaseServicesService, private route:Router) { }
 
   ngOnInit(): void {
+    
     this.getUser()
+  }
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    if (setPageSizeOptionsInput) {
+      this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+    }
   }
   getUser(){
     this.base.getUsers().subscribe((res:any)=>{
-      const{data}=res
-      this.user=data
-      return console.log (data)
+      console.log(res)
+      const{data,nextPage}=res
+      this.user=data,nextPage
+      return console.log (data,nextPage)
+      
     })
   }
 
