66d8a54043450dc2a8cee95f86a2798f6c017458
Santiago Carranza <santiago.c.trjo@gmail.com>
2022-11-12 13:33:17 -0300

    2

---
+++ src/app/shared/components/formEndPointTransaction/form-end-point-transaction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ab-form-end-point-transaction',
+  templateUrl: './form-end-point-transaction.component.html',
+  styleUrls: ['./form-end-point-transaction.component.scss']
+})
+export class FormEndPointTransactionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
